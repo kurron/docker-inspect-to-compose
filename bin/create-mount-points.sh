8a1aafe25e68dfb144f2a5cce20d8e5fa05c2b57
Ron Kurr <kurr@kurron.org>
2015-11-04 15:09:14 +0000

    Added mount point creation script

---
+++ bin/create-mount-points.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo mkdir -p /srv/courses
+sudo mkdir -p /opt/dockerbuild/mon-e/hacks
+sudo mkdir -p /srv/media 
+sudo mkdir -p /srv/log/tlocategories 
+sudo mkdir -p /srv/courses/courses 
+sudo mkdir -p /srv/media/courses 
+sudo mkdir -p /srv/tlomli 
+sudo mkdir -p /opt/tlomongodb
